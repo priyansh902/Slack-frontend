218288a828acaa63a6eabbb929301e40037b1761
Priyanshu Kumar <pk953666@gmail.com>
2026-03-17 20:18:37 +0530

    app colors

---
+++ lib/core/theme/colors.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Primary Colors
+  static const Color primaryBlue = Color(0xFF2196F3);
+  static const Color primaryDark = Color(0xFF1976D2);
+  static const Color primaryLight = Color(0xFFBBDEFB);
+  
+  // Secondary Colors
+  static const Color secondaryGreen = Color(0xFF4CAF50);
+  static const Color secondaryRed = Color(0xFFF44336);
+  static const Color secondaryOrange = Color(0xFFFF9800);
+  
+  // Neutral Colors
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color black = Color(0xFF000000);
+  static const Color greyLight = Color(0xFFF5F5F5);
+  static const Color greyMedium = Color(0xFFE0E0E0);
+  static const Color greyDark = Color(0xFF9E9E9E);
+  
+  // Text Colors
+  static const Color textPrimary = Color(0xFF212121);
+  static const Color textSecondary = Color(0xFF757575);
+  static const Color textHint = Color(0xFFBDBDBD);
+  
+  // Background
+  static const Color backgroundLight = Color(0xFFFAFAFA);
+  static const Color backgroundDark = Color(0xFF303030);
+  
+  // Status Colors
+  static const Color success = Color(0xFF4CAF50);
+  static const Color error = Color(0xFFF44336);
+  static const Color warning = Color(0xFFFF9800);
+  static const Color info = Color(0xFF2196F3);
+}
